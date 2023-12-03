0b64559157dd4179eed698d96473634abdd2cee9
Atlas <antonio.claudino@fido.com.vc>
2023-12-03 12:11:50 -0300

    code refactoring

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,6 +1,75 @@
+import React, { useState } from "react";
 import "./App.css";
-import { Calculator } from "./Components/Calculator";
 
 export const App = () => {
-  return <Calculator />;
+  const [input, setInput] = useState("");
+  const buttons = [
+    "AC",
+    "DEL",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "*",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "00",
+    "0",
+    ".",
+    "=",
+  ];
+
+  const handleClick = (value) => {
+    if (value === "=") {
+      setInput(eval(input).toString());
+    } else if (value === "AC") {
+      setInput("");
+    } else if (value === "DEL") {
+      setInput(input.slice(0, -1));
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  const getButtonClass = (button) => {
+    if (["AC", "DEL", "%"].includes(button)) {
+      return "buttonGrey";
+    } else if (["/", "*", "-", "+", "=", "."].includes(button)) {
+      return "operator";
+    } else {
+      return "number";
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="calculator">
+        <input
+          type="text"
+          id="inputBox"
+          value={input}
+          placeholder="0"
+          readOnly
+        />
+        <div>
+          {buttons.map((button, index) => (
+            <button
+              key={index}
+              className={`button ${getButtonClass(button)}`}
+              onClick={() => handleClick(button)}
+            >
+              {button}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
